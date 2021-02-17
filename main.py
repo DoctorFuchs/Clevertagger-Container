299f48b85a5617e1f8f6e019a61dcfd25314364f
anetschka <ak47schumann@gmail.com>
2021-02-17 15:54:55 +0100

    now we get smor analyses: not elegant, but works

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ app = FastAPI()
 logger = logging.getLogger('CleverTaggerLogger')
 
 @app.post("/smor")
-def get_smor():
-    subprocess.run("echo 'Vermittlungsgespräche'  | fst-infl2 ../../data/zmorge/zmorge-20150315-smor_newlemma.ca > /clevertagger/clevertagger/test.txt", shell=True)
-    with codecs.open('test.txt', 'rb', 'utf8') as f:
-        lines = f.readlines()
-        print(lines)
+def get_smor(text: str):
+    if not text or text.isspace():
+        logger.error('no content delivered to get_tags')
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST)
+    try:
+        subprocess.run("echo '" + text + "' | fst-infl2 ../../data/zmorge/zmorge-20150315-smor_newlemma.ca > /clevertagger/clevertagger/test.txt", shell=True)
+        with codecs.open('test.txt', 'rb', 'utf8') as f:
+            return f.readlines()
+    except Exception as e:
+        logger.exception('get_smor threw exception: ' + str(e))
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
 
 @app.post("/clever")
 def get_tags(text: str):
